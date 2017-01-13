004599a39daa86037925584f382f8b12739c80a9
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-12 22:16:23 -0200

    header banner

---
+++ responsive-SPA/navbar-animation-fix.js
@@ -0,0 +1,9 @@
+var banner = $('.topCasaFina-banner');
+
+$('#collapse-navbar').on('show.bs.collapse', function() {
+	banner.css('transform', 'translate(-50%, 10%)');
+});
+
+$('#collapse-navbar').on('hide.bs.collapse', function() {
+	banner.css('transform', 'translate(-50%, -50%)');
+});
